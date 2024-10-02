e27573df339c6b4cf41694c68a415e7f5e6b1015
brian419 <json10@crimson.ua.edu>
2024-10-02 11:45:46 -0500

    Modifying functionality to reflect assignment requirements. Need to work on changing text color to white instead of black and make sure a random image preloads as the background

--- Fall2024-Assignment2-json10/wwwroot/script.js
+++ Fall2024-Assignment2-json10/wwwroot/script.js
@@ -1,16 +1,32 @@
-// to ensure the dom is fully loaded before running scripts
 $(document).ready(function () {
+    var backgroundToggle = true;
 
-    // when using bing api search
-    function apiSearch() {
+    function getRandomImage(callback) {
+        var apiKey = '4V73spW5KP6UUG6hDMaKiHtD8cLCfBmCvmh6xPxn3Cs';  
+
+        $.getJSON(`https://api.unsplash.com/photos/random?client_id=${apiKey}`, function (data) {
+            callback(data.urls.full);  
+        });
+    }
+
+    function changeBackgroundImage() {
+        getRandomImage(function (newImage) {
+            $('body').css('background-image', `url(${newImage})`);
+        });
+    }
+
+    $('#searchEngineName').on('click', function () {
+        changeBackgroundImage();
+    });
+
+    function apiSearch(isLucky = false) {
         var params = {
-            'q': $('#query').val(), 
-            'count': 10,            
-            'offset': 0,            
-            'mkt': 'en-us'         
+            'q': $('#query').val(),
+            'count': 10,
+            'offset': 0,
+            'mkt': 'en-us'
         };
 
-        // making the api call
         $.ajax({
             url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
             type: 'GET',
@@ -23,17 +39,21 @@ $(document).ready(function () {
                     var len = data.webPages.value.length;
                     var results = '<h2>Search Results:</h2>';
 
-                    // looping results and building the content
                     for (var i = 0; i < len; i++) {
                         var result = data.webPages.value[i];
                         results += `<p><a href="${result.url}" target="_blank">${result.name}</a>: ${result.snippet}</p>`;
                     }
 
-                    $('#searchResults').html(results).dialog({
-                        title: "Search Results",
-                        width: 600,
-                        modal: true
-                    });
+                    if (isLucky) {
+                        
+                        window.location.href = data.webPages.value[0].url;
+                    } else {
+                        $('#searchResults').html(results).dialog({
+                            title: "Search Results",
+                            width: 600,
+                            modal: true
+                        });
+                    }
                 } else {
                     $('#searchResults').html('<p>No results found.</p>').dialog({
                         title: "Search Results",
@@ -47,10 +67,14 @@ $(document).ready(function () {
             });
     }
 
-    // to bind the search button click event to the apiSearch function
-    $('#searchButton').on('click', apiSearch);
+    $('#searchButton').on('click', function () {
+        apiSearch();
+    });
 
-    // shows current time when 'time' button is clicked
+    $('#luckyButton').on('click', function () {
+        apiSearch(true); 
+    });
+
     $('#timeButton').on('click', function () {
         var currentTime = new Date();
         var formattedTime = currentTime.getHours() + ":" + (currentTime.getMinutes() < 10 ? '0' : '') + currentTime.getMinutes();
@@ -60,15 +84,5 @@ $(document).ready(function () {
             width: 300,
             modal: true
         });
-    });
-
-    // changes the background when clicking the search engine name
-    $('#searchEngineName').on('click', function () {
-        var currentBackground = $('body').css('background-image');
-        var newBackground = currentBackground.includes('unsplash.com') ?
-            'url(https://source.unsplash.com/random/1600x900)' :
-            'url(https://source.unsplash.com/random/1600x901)';  // toggles between two random Unsplash images
-
-        $('body').css('background-image', newBackground);
     });
 });
